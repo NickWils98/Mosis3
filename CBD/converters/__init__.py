8f9d00e566977002cf3056ef1ff9da7a3d51fda1
NickWils98 <nick.wils@student.uantwerpen.be>
2022-11-09 15:18:08 +0100

    Src map

--- CBD/converters/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This package contains a set of independent modules pertaining the conversion
-to and from CBDs.
-"""
-
--- CBD/converters/latexify/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Converts a CBD network into a system of equations.
-"""
-
-from CBD.converters.latexify.CBD2Latex import *
-from CBD.converters.latexify.functions import *
