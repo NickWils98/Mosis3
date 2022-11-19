76a09a17d9e06e806a9096c11f4f1584333c4675
NickWils98 <nick.wils@student.uantwerpen.be>
2022-11-19 06:11:51 +0100

    util

---
+++ util.py
@@ -0,0 +1,33 @@
+
+from CBD.simulator import Simulator
+import matplotlib.pyplot as plt
+from CBD.converters.latexify.CBD2Latex import CBD2Latex
+
+
+def checkValitidyLatex(model):
+    cbd2latex = CBD2Latex(model, show_steps=True, render_latex=False)
+
+    cbd2latex.simplify()
+
+    # print the resulting equations
+    print("RESULT IS:")
+    print(cbd2latex.render())
+
+
+def run(cbd, num_steps, delta_t, title, RKF=False, filename=None):
+    sim = Simulator(cbd)
+    if RKF == False:
+        sim.setDeltaT(delta_t)
+    sim.run(num_steps)
+
+    data = cbd.getSignalHistory('OUT1')
+    x, y = [x for x, _ in data], [y for _, y in data]
+
+    plt.plot(x, y)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.title(title)
+    if filename is None:
+        plt.show()
+    else:
+        plt.savefig(filename)
